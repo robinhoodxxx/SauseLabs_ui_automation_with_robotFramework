73d657a885ddbfb5fa5496aff2a8a18aea73dfc7
robinhoodxxx <karrothutanesh016@gmail.com>
2025-10-29 23:02:01 +0530

    fix: simplify output directory handling in run.bat

--- run.bat
+++ run.bat
@@ -15,17 +15,10 @@ if "%hh:~0,1%"==" " set hh=0%hh:~1,1%
 set "reportdir=Reports"
 set "outdir=%reportdir%\%yyyy%%mm%%dd%_%hh%%min%%ss%"
 python -m robot -A robot.options --outputdir %outdir% --variable SCREENSHOT_DIR:%outdir%\screenshots %*
+
 REM Copy the log and report files into the output directory
-copy "%outdir%\output.xml" "%reportdir%\"
-copy "%outdir%\log.html" "%reportdir%\"
-copy "%outdir%\report.html" "%reportdir%\"
-if exist "%reportdir%\screenshots\" (
-    echo Screenshots directory exists.
-) else (
-    echo Screenshots directory does not exist. Creating it.
-    mkdir "%reportdir%\screenshots\"
-    )
-copy "%outdir%\screenshots\*" "%reportdir%\screenshots\"
-REM Create a zip file of the output directory using PowerShell
+xcopy "%outdir%" "%reportdir%" /E /I /Y
+
+REM  Create a zip file of the output directory using PowerShell
 powershell -Command "Compress-Archive -Path '%outdir%\*' -DestinationPath '%reportdir%\report.zip' -Force"
 endlocal
