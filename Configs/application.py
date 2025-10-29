10106c7df057b03af3f8d2ee10ed32329d2d463b
robinhoodxxx <karrothutanesh016@gmail.com>
2025-10-29 23:03:04 +0530

    feat: add application configuration settings including browser, timeout, and credentials

---
+++ Configs/application.py
@@ -0,0 +1,13 @@
+# config_vars.py
+
+# Common Application Settings
+BROWSER = "Chrome "
+NORMAL_TIMEOUT = "15s"
+CHROME_HEADLESS = True
+# Credentials (Use environment variables or secrets manager in real projects!)
+LOGIN_URL =   "https://www.saucedemo.com/"
+User_name =   "standard_user"
+Pass_word =   "secret_sauce"
+
+# File Paths
+DOWNLOAD_DIR = "/temp/downloads"
